f0bb53cfd0bf1df61bc7be88d82439e543c7bfff
kamran saeidi <pakan0020@gmail.com>
2021-04-04 13:37:30 +0430

    Manage categories

--- app/routes/web/admin.js
+++ app/routes/web/admin.js
@@ -1,47 +1,49 @@
 const express = require('express');
 const router = express.Router();
 
-//controller
-const adminController = require('app/http/controllers/admin/admincontroller')
-const courseController = require('app/http/controllers/admin/coursecontroller')
+// Controllers
+const adminController = require('app/http/controllers/admin/adminController');
+const courseController = require('app/http/controllers/admin/courseController');
 const episodeController = require('app/http/controllers/admin/episodeController');
 const commentController = require('app/http/controllers/admin/commentController');
+const categoryController = require('app/http/controllers/admin/categoryController');
 
-//validators
-const courseValidator = require('app/http/validators/coursevalidator')
+// validators 
+const courseValidator = require('app/http/validators/courseValidator');
 const episodeValidator = require('app/http/validators/episodeValidator');
+const categoryValidator = require('app/http/validators/categoryValidator');
 
-//Helpers
+// Helpers
 const upload = require('app/helpers/uploadImage');
 
-//Middlewares
-const convertFileToField = require('app/http/middleware/convertfiletofield')
+// Middlewares
+const convertFileToField = require('app/http/middleware/convertFileToField')
 
-router.use((req, res, next) => {
+router.use((req , res , next) => {
     res.locals.layout = "admin/master";
     next();
 })
 
 // Admin Routes
-router.get('/', adminController.index);
+router.get('/' , adminController.index);
 
-//Course Routes
-router.get('/courses', courseController.index);
-router.get('/courses/create', courseController.create);
-router.post('/courses/create',
-    upload.single('images'),
-    convertFileToField.handle,
-    courseValidator.handle(),
+// Course Routes
+router.get('/courses' , courseController.index);
+router.get('/courses/create' , courseController.create);
+router.post('/courses/create' ,
+    upload.single('images') ,
+    convertFileToField.handle ,
+    courseValidator.handle() ,
     courseController.store
 );
-router.get('/courses/:id/edit', courseController.edit);
-router.put('/courses/:id',
-upload.single('images'),
-convertFileToField.handle,
-courseValidator.handle(),
-courseController.update
+router.get('/courses/:id/edit' , courseController.edit);
+router.put('/courses/:id' ,
+    upload.single('images') ,
+    convertFileToField.handle ,
+    courseValidator.handle() ,    
+    courseController.update
 );
-router.delete('/courses/:id', courseController.destroy);
+router.delete('/courses/:id' , courseController.destroy);
 
 // Episode Routes
 router.get('/episodes' , episodeController.index);
@@ -51,7 +53,15 @@ router.get('/episodes/:id/edit' , episodeController.edit);
 router.put('/episodes/:id' , episodeValidator.handle() , episodeController.update );
 router.delete('/episodes/:id' , episodeController.destroy);
 
-// Comment Routes
+// Category Routes
+router.get('/categories' , categoryController.index);
+router.get('/categories/create' , categoryController.create);
+router.post('/categories/create' , categoryValidator.handle() , categoryController.store );
+router.get('/categories/:id/edit' , categoryController.edit);
+router.put('/categories/:id' , categoryValidator.handle() , categoryController.update );
+router.delete('/categories/:id' , categoryController.destroy);
+
+//Comment Routes
 router.get('/comments/approved' , commentController.approved);
 router.get('/comments' , commentController.index);
 router.put('/comments/:id/approved' , commentController.update );
